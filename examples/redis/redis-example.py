afb16555d6683cda73947ecf2032daf819f8dc01
Jamie Lennox <jamie@vibrato.com.au>
2019-05-08 23:25:56 +1000

    Touch up redis example
    Give it some validators, but make the client accessor a bit more
    realistic.

--- examples/redis/redis-example.py
+++ examples/redis/redis-example.py
@@ -2,6 +2,7 @@ import logging
 
 import configurer
 from configurer.readers.environ import EnvironmentReader
+from configurer import validators
 import redis
 
 logging.basicConfig(level=logging.DEBUG)
@@ -11,22 +12,37 @@ class RedisComponent(configurer.Component):
 
     host = configurer.Option('host',
                              parser=str,
+                             validator=[
+                                 validators.Required(),
+                             ],
                              default='localhost')
 
     port = configurer.Option('port',
                              parser=int,
+                             validator=[
+                                 validators.Required(),
+                                 validators.GreaterThan(0),
+                                 validators.LowerThan(65536),
+                             ],
                              default=6379)
 
-    password = configurer.Option('password',
-                                 parser=str)
+    password = configurer.Option('password', parser=str)
 
+    def __init__(self, *args, **kwargs):
+        super(RedisComponent, self).__init__(*args, **kwargs)
+        self._client = None
+
+    @property
     def client(self):
-        kwargs = {'host': self.host, 'port': self.port}
+        if not self._client:
+            kwargs = {'host': self.host, 'port': self.port}
 
-        if self.password is not configurer.NO_VALUE:
-            kwargs['password'] = self.password
+            if self.password is not configurer.NO_VALUE:
+                kwargs['password'] = self.password
 
-        return redis.Redis(**kwargs)
+            self._client = redis.Redis(**kwargs)
+
+        return self._client
 
 
 cm = configurer.ConfigManager([
@@ -34,10 +50,8 @@ cm = configurer.ConfigManager([
 ])
 
 redis_loader = RedisComponent(cm, ['Redis'])
-client = redis_loader.client()
-
-client.set('tester', 'hello'.encode('utf-8'))
-assert client.get('tester').decode('utf-8') == 'hello'
+redis_loader.client.set('tester', 'hello'.encode('utf-8'))
+assert redis_loader.client.get('tester').decode('utf-8') == 'hello'
 
 
 print("**** SUCCESS ****")
